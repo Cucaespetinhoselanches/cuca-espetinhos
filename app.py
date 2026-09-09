85f73f676e4f665a811e74ec476293b65f0e2bc0
Cucaespetinhoselanches <valdirsilva4073@gmail.com>
2026-09-09 17:10:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -114,59 +114,59 @@ menu = {
     },
     "Cerveja Original 300ml Garrafinha": {
         "preco": 6.00,
-        "imagem": "https://images.unsplash.com/photo-1608270586620-248524c67de9?w=400",
+        "imagem": "https://th.bing.com/th/id/OIP.yv46CREzjTxIfnQpp9lU5wHaHa?w=184&h=187&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     "Cerveja Budweiser 300ml Garrafinha": {
         "preco": 6.00,
-        "imagem": "https://images.unsplash.com/photo-1608270586620-248524c67de9?w=400",
+        "imagem": "https://th.bing.com/th/id/OIP.yv46CREzjTxIfnQpp9lU5wHaHa?w=184&h=187&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     "Cerveja Antarctica 300ml Garrafinha": {
         "preco": 5.00,
-        "imagem": "https://images.unsplash.com/photo-1608270586620-248524c67de9?w=400",
+        "imagem": "https://th.bing.com/th/id/OIP.yv46CREzjTxIfnQpp9lU5wHaHa?w=184&h=187&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     "Cerveja Brahma 300ml Garrafinha": {
         "preco": 5.00,
-        "imagem": "https://images.unsplash.com/photo-1608270586620-248524c67de9?w=400",
+        "imagem": "https://th.bing.com/th/id/OIP.yv46CREzjTxIfnQpp9lU5wHaHa?w=184&h=187&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     "Cerveja Império 300ml Garrafinha": {
         "preco": 5.00,
-        "imagem": "https://images.unsplash.com/photo-1608270586620-248524c67de9?w=400",
+        "imagem": "https://th.bing.com/th/id/OIP.yv46CREzjTxIfnQpp9lU5wHaHa?w=184&h=187&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     "Cerveja Skol 269ml Lata": {
         "preco": 5.00,
-        "imagem": "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400",
+        "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
     },
     "Cerveja Budweiser 269ml Lata": {
         "preco": 6.00,
-        "imagem": "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400",
+        "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
     },
     "Cerveja Original 269ml Lata": {
         "preco": 6.00,
-        "imagem": "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400",
+        "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
     },
     "Cerveja Amstel 269ml Lata": {
         "preco": 5.00,
-        "imagem": "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400",
+        "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
     },
     "Cerveja Império 269ml Lata": {
         "preco": 5.00,
-        "imagem": "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400",
+        "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
     },
     "Cerveja Brahma Duplo Malte 269ml Lata": {
         "preco": 6.00,
-        "imagem": "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400",
+        "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
     },
     "Cerveja Long Neck Heineken": {
         "preco": 10.00,
-        "imagem": "https://images.unsplash.com/photo-1608270586620-248524c67de9?w=400",
+        "imagem": "https://images.unsplash.com/photo-1618885472179-5e474019f2a9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z2FycmFmYSUyMGRlJTIwY2VydmVqYXxlbnwwfHwwfHx8MA%3D%3D",
     },
     "Cerveja Long Neck Budweiser": {
         "preco": 10.00,
-        "imagem": "https://images.unsplash.com/photo-1608270586620-248524c67de9?w=400",
+        "imagem": "https://images.unsplash.com/photo-1587669284207-e8ee0fc74144?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z2FycmFmYSUyMGRlJTIwY2VydmVqYXxlbnwwfHwwfHx8MA%3D%3D",
     },
     "Cerveja Long Neck Corona": {
         "preco": 10.00,
-        "imagem": "https://images.unsplash.com/photo-1608270586620-248524c67de9?w=400",
+        "imagem": "https://images.unsplash.com/photo-1600213903598-25be92abde40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FycmFmYSUyMGRlJTIwY2VydmVqYXxlbnwwfHwwfHx8MA%3D%3D",
     },
     "Energético Monster 473ml": {
         "preco": 14.00,
@@ -174,15 +174,15 @@ menu = {
     },
     "Cerveja Amstel 350ml Lata": {
         "preco": 6.00,
-        "imagem": "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400",
+        "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
     },
     "Cerveja Império 350ml Lata": {
         "preco": 6.00,
-        "imagem": "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400",
+        "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
     },
     "Cerveja Brahma Duplo Malte 350ml Lata": {
         "preco": 7.00,
-        "imagem": "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400",
+        "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
     },
 }
 
