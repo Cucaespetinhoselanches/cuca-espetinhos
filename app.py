5c2bfd07d239386bcd889aa1a0e95876de86e690
Cucaespetinhoselanches <valdirsilva4073@gmail.com>
2026-09-10 21:29:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -519,15 +519,15 @@ menu_categorias = {
         },
         "Cerveja Amstel 350ml Lata": {
             "preco": 6.00,
-            "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
+            "imagem": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqQ_3JZ9bzr9LODFFj1Lps0OroT6jJ_PrC24CiZ0nEHg&s=10",
         },
         "Cerveja Império 350ml Lata": {
             "preco": 6.00,
-            "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
+            "imagem": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXKTXeRvTYQBjWEX-eei2xutbAx97LRhZnpOrEEQ_5Dg&s=10",
         },
         "Cerveja Brahma Duplo Malte 350ml Lata": {
             "preco": 7.00,
-            "imagem": "https://tse4.mm.bing.net/th/id/OIP.Cqa-Rx_ea-YoraLE7dVVpQHaFj?r=0&pid=ImgDet&w=204&h=153&c=7&o=7&rm=3",
+            "imagem": "https://hortifrutibr.vtexassets.com/arquivos/ids/173202/Cerveja-Brahma-Duplo-Malte-Lata-Sleek-350Ml.png?v=639239767332900000",
         },
     },
 }
